fd57f233bce88c4489dd63fc92cfcdcd114210ff
MateuszLaszczyk <mati225511@gmail.com>
2025-08-21 15:18:29 +0200

    added annotations and tests descriptions

--- src/test/java/CreateListOnBoardTest.java
+++ src/test/java/CreateListOnBoardTest.java
@@ -1,5 +1,10 @@
 import BaseTest.BaseTest;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import io.restassured.http.ContentType;
+import jdk.jfr.Description;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +13,11 @@ import static io.restassured.RestAssured.given;
 public class CreateListOnBoardTest extends BaseTest {
 
     @Test
-    @DisplayName("This test create a list on a board")
+    @Epic("Trello API")
+    @Feature("List Management")
+    @DisplayName("Create a new list on the board")
+    @Description("This test verifies that a user can successfully create a new list on a Trello board using the boardId.")
+    @Severity(SeverityLevel.CRITICAL)
     public void createListOnBoardTest() {
         String listId = given()
                 .queryParam("key", trelloKey)
--- src/test/java/UpdateBoardNameTest.java
+++ src/test/java/UpdateBoardNameTest.java
@@ -1,5 +1,11 @@
 import BaseTest.BaseTest;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import io.restassured.http.ContentType;
+import jdk.jfr.Description;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -7,6 +13,11 @@ import static io.restassured.RestAssured.given;
 public class UpdateBoardNameTest extends BaseTest {
 
     @Test
+    @Epic("Trello API")
+    @Feature("Board Management")
+    @DisplayName("Update board name")
+    @Description("Verifies that an existing board's name can be successfully updated.")
+    @Severity(SeverityLevel.NORMAL)
     void updateBoardName() {
         boardId =
                 given()
